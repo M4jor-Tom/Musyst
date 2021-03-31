94721267fd69b72190d9c5798b1a9105d2ddb533
Tom <tom-vautray@hotmail.fr>
2021-03-31 06:25:48 +0200

    Test of fetching grandson's songs
    works

--- src/mainPackage/MainClass.java
+++ src/mainPackage/MainClass.java
@@ -1,9 +1,12 @@
 package mainPackage;
 
+import mediaTier.*;
+
 public class MainClass
 {
 	public static void main(String[] args)
 	{
-
+		MediaInterface musicTitlesFetcher = new GoogleMusicScraper("grandson");
+		musicTitlesFetcher.getResources();
 	}
 }
