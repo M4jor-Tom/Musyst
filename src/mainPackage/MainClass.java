9bc49a1d861c58c9e6ecd03e73d95dd0f3f306ed
Tom <tom-vautray@hotmail.fr>
2021-04-01 09:03:22 +0200

    corrected previous commit

--- src/mainPackage/MainClass.java
+++ src/mainPackage/MainClass.java
@@ -1,12 +1,14 @@
 package mainPackage;
 
 import mediaTier.*;
+import resourcePackage.AudioResource;
 
 public class MainClass
 {
 	public static void main(String[] args)
 	{
-		MediaInterface musicTitlesFetcher = new GoogleMusicScraper("grandson");
-		musicTitlesFetcher.getResources();
+		MediaInterface<AudioResource> musicsScraper = new GoogleMusicScraper("grandson");
+		for(AudioResource resource: musicsScraper.getResources())
+			System.out.println(resource);
 	}
 }
