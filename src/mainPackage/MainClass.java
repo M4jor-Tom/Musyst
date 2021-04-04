fcc7de5201e23e8fcd0fb66c8b33f7f907531331
Tom <tom-vautray@hotmail.fr>
2021-04-05 00:41:15 +0200

    Corrected ResourceType in main

--- src/mainPackage/MainClass.java
+++ src/mainPackage/MainClass.java
@@ -7,8 +7,8 @@ public class MainClass
 {
 	public static void main(String[] args)
 	{
-		MediaInterface<AudioResource> musicsScraper = new GoogleMusicScraper("grandson");
-		for(AudioResource resource: musicsScraper.getResources())
+		MediaInterface<MusicResource> musicsScraper = new GoogleMusicScraper("grandson");
+		for(MusicResource resource: musicsScraper.getResources())
 			System.out.println(resource);
 	}
 }
