0572f95bddd5f8684df5363952937f249021fbb1
Tom <tom-vautray@hotmail.fr>
2021-04-07 16:40:53 +0200

    Add of findAlbum to interface MediaInterface

--- src/mediaTier/MediaInterface.java
+++ src/mediaTier/MediaInterface.java
@@ -3,10 +3,12 @@ package mediaTier;
 import java.util.ArrayList;
 
 import resourcePackage.Album;
+import resourcePackage.Author;
 import resourcePackage.Resource;
 
 public interface MediaInterface<ResourceType extends Resource>
 {
+	public Album findAlbum(Author author, String albumName);
 	public ArrayList<Album> findAlbums(String authorName);
 	public ArrayList<Album> getAlbums();
 }
