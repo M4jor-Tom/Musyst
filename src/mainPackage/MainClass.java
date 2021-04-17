a210ff6b649463d0b756aa6d6b8849817173e8f2
Tom <tom-vautray@hotmail.fr>
2021-04-17 12:47:43 +0200

    #1 Implementation of 'Versionable' artchitecture

--- src/mainPackage/MainClass.java
+++ src/mainPackage/MainClass.java
@@ -2,14 +2,46 @@ package mainPackage;
 
 import musicFindystPackage.GoogleMusicScraper;
 import musicFindystPackage.MusicFindystInterface;
+import musicFindystPackage.MusicFindystVersionable;
+import musystConfig.MusystVersionable;
+
+import java.util.HashMap;
+
 import logicTier.*;
 import resourcePackage.Album;
 import resourcePackage.MusicResource;
+import resourcePackage.ResourcystVersionable;
+import versionystPackage.Versionable;
+import versionystPackage.Versionyst;
 
 public class MainClass
 {
+	public static void versionsCheck()
+	{
+		//Variables initialization
+		HashMap<String, Integer> existingDependencies = new HashMap<>();
+		Versionable[] versionables =
+		{
+			new MusystVersionable(),
+			new MusicFindystVersionable(),
+			new ResourcystVersionable(),
+			new Versionyst()
+		};
+		
+		//Versions setting
+		existingDependencies.put("MusicFindystVersionable", versionables[1].getVersionId());
+		existingDependencies.put("ResourcystVersionable", versionables[2].getVersionId());
+		existingDependencies.put("Versionyst", versionables[3].getVersionId());
+
+		//Versions checking
+		for(Versionable versionable: versionables)
+			versionable.checkSubPackagesVersions(existingDependencies);
+	}
+	
 	public static void main(String[] args)
 	{
+		versionsCheck();
+		
 		MusicFindystInterface musicInterface = new GoogleMusicScraper();
 		LogicInterface logic = new Logic(musicInterface);
 		
